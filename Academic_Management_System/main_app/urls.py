0a37328cfe6370f15cb43a9b321f6979edf0c49d
NguyenDangKhoa04072004 <161735366+NguyenDangKhoa04072004@users.noreply.github.com>
2024-03-18 19:45:47 +0700

    Delete Academic_Management_System directory

--- Academic_Management_System/main_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views, AdminViews, StudentViews, TeacherViews
-urlpatterns = [
-    # Login Path
-    path('', views.index, name="index"),
-    # Student Path
-    # Teacher Path 
-    # Admin Path
-]
